8b882c71f39127ce03cc349051cfc81b9ad6b662
Garox7 <giuseppe.garozzo2@icloud.com>
2022-11-02 21:02:42 +0100

    Improvements

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,20 @@
+let difficultyLevel = document.getElementById('select-difficulty').value;
+const playButton = document.querySelector('.header__playgame');
 const gridElement = document.querySelector('.field__grid');
-let difficultySelector = document.getElementById('select-difficulty');
-const playButton = document.querySelector('.header__playgame'); 
-console.log(difficultySelector);
 
 playButton.addEventListener('click', function(){
-
-    let difficultyLevel = difficultySelector.value;
-    console.log(difficultyLevel);
-
     gridElement.replaceChildren();
+    console.log('mi hai cliccato');
     
     if (difficultyLevel == '1') {
         numberCell = 100;
-        console.log(numberCell);
         
     } else if (difficultyLevel == '2') {
         numberCell = 81;
-        console.log(numberCell);
     
     } else if (difficultyLevel == '3') {
         numberCell = 49;
-        console.log(numberCell);
     }
-
     
     for (let i = 1; i <= numberCell; i++) {
         const cellElement = document.createElement('div');
@@ -43,6 +35,11 @@ playButton.addEventListener('click', function(){
     
         gridElement.append(cellElement);
         cellElement.append([i]);
+
+
+        cellElement.addEventListener ('click', function() {
+            this.classList.toggle('active');
+        })
     }
 })
 
